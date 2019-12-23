e88554a5a9dcbcff28b5c3beeac26339de835d80
Leo Basov <basov_leo@web.de>
2019-12-23 23:36:20 +0100

    Modified stuff.

--- src/io/writer.f90
+++ src/io/writer.f90
@@ -1,3 +1,23 @@
-module writer
+module class_writer
    implicit none
-end module writer
+   private
+
+   type, public :: Writer
+   contains
+      final :: destructor
+   end type Writer
+
+   interface Writer
+      procedure :: constructor
+   end interface writer
+
+contains
+   function constructor(file_name) result(this)
+      character :: file_name
+      type(Writer) :: this
+   end function constructor
+
+   subroutine destructor(this)
+      type(Writer) :: this
+   end subroutine destructor
+end module class_writer
