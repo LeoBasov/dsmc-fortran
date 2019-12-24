d1ce4355101d25ce5b077f75a00c35269c87b68c
Leo Basov <basov_leo@web.de>
2019-12-24 10:10:11 +0100

    Extended writer.

--- src/io/writer.f90
+++ src/io/writer.f90
@@ -2,7 +2,10 @@ module class_writer
    implicit none
    private
 
+   integer :: unit_module = 1
+
    type, public :: Writer
+      integer, private :: unit_loc
    contains
       final :: destructor
    end type Writer
@@ -15,9 +18,16 @@ contains
    function constructor(file_name) result(this)
       character :: file_name
       type(Writer) :: this
+
+      this%unit_loc = unit_module
+      unit_module = unit_module + 1
+
+      open (unit=this%unit_loc, file=file_name)
    end function constructor
 
    subroutine destructor(this)
       type(Writer) :: this
+
+      close (this%unit_loc)
    end subroutine destructor
 end module class_writer
