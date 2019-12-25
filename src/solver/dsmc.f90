0f65a2520ade69e6dc1a33267ca911960f269923
Leo Basov <basov_leo@web.de>
2019-12-25 12:16:08 +0100

    extended dsmc

--- src/solver/dsmc.f90
+++ src/solver/dsmc.f90
@@ -1,4 +1,18 @@
 module dsmc_solver
+   use class_data
    implicit none
+   private
+
+   type, public :: dsmc
+   contains
+      procedure :: execute => execute_dsmc
+   end type dsmc
+
+contains
+   subroutine execute_dsmc(this, particles)
+      class(dsmc), intent(in) :: this
+      type(Data), intent(inout) :: particles
+
+   end subroutine execute_dsmc
 
 end module dsmc_solver
