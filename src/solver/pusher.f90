54f497a40a237717b43b761f1457fb84b8c27906
Leo Basov <basov_leo@web.de>
2019-12-26 15:08:47 +0100

    Extended pusher

--- src/solver/pusher.f90
+++ src/solver/pusher.f90
@@ -6,14 +6,15 @@ contains
    subroutine push_particles(particles)
       type(data), intent(inout) :: particles
 
-      call push(particles%positions, particles%velocities, particles%time_step)
+      call push(particles%positions, particles%old_positions, particles%velocities, particles%time_step)
    end subroutine push_particles
 
-   subroutine push(positions, velocities, time_step)
-      real, dimension(:, :), intent(inout) :: positions
+   subroutine push(positions, old_positions, velocities, time_step)
+      real, dimension(:, :), intent(inout) :: positions, old_positions
       real, dimension(:, :), intent(in) :: velocities
       real, intent(in) :: time_step
 
+      old_positions = positions
       positions = positions + velocities*time_step
    end subroutine push
 
